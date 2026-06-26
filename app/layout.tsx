a301045c8314877b1fa14e7ff213f15b079ee8c9
Sten Hiiesalu <stenhiiesalu@MacBookAir.lan>
2026-06-26 21:12:27 +0300

    add microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,18 @@ export default function RootLayout({
         <body>
   {children}
   <GoogleAnalytics gaId="G-5W1JWW9YCR" />
+<script
+  type="text/javascript"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "xd6mym2w1r");
+    `,
+  }}
+/>
 </body>
       </html>
     </ClerkProvider>
